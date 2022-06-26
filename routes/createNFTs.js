7d086dca762240dc9541f9a7f2d8a07fe06eb936
metagabbar <metagabbar@gmail.com>
2022-06-25 23:35:57 -0400

    Add parent endpoint, README

--- routes/createNFTs.js
+++ routes/createNFTs.js
@@ -11,22 +11,33 @@ export async function createNFTs(req, res, err) {
 
         let name = data.name
         let symbol = data.symbol
-        let collection = data.collection
+        let childCollectionMetadata = data.child_metadata
+        let parentHolders = data.parent_holders
+
+        if (!name || !symbol || !childCollectionMetadata || !parentHolders) {
+            res.status(400).json({"error":"name, symbol, child_metadata, parent_holders should be passed"})
+            return
+        }
+        if (childCollectionMetadata.length !== parentHolders.length) {
+            res.status(400).json({"error":"child_metadata and parent_holders should be same length"})
+            return
+        }
 
         // let contractAddress = await createNFTContract(name, symbol)
         let contractAddress = "0x80d2ab2b94969204ccfc86267ef09d8010e1b8b8"
-        let uploadedMetadataList = await augmentAndUploadMetadataToIPFS(collection)
+        let uploadedMetadataList = await augmentAndUploadMetadataToIPFS(childCollectionMetadata)
         let mintedCollection = await mintAllNftsInCollection(contractAddress, uploadedMetadataList)
 
         let mongoClient = await getMongoDbClient();
         let createdJob = await mongoClient.collection('jobs').insertOne({
             name,
             symbol,
-            mintedCollection
+            mintedCollection,
+            parentHolders
         })
         console.log(createdJob)
 
-        return res.json({name, symbol, mintedCollection, ...createdJob})
+        return res.json({name, symbol, childCollection:mintedCollection, parentHolders, ...createdJob})
     } catch (err) {
         console.error(`${err}`)
         return res.status(400).json({"error":err})
