7c2196df2881306b6d7ab06e4462852d4c248a3e
metagabbar <metagabbar@gmail.com>
2022-06-26 04:27:09 -0400

    More changes for mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -67,8 +67,8 @@ module.exports = {
         privateKeys: [process.env.PRIVATE_KEY],
         providerOrUrl: process.env.MATIC_PROVIDER_URL
       }),
-      network_id: 80001,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      network_id: 137,       //  id
+      gas: 5500000,        //  has a lower block limit than mainnet
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
       timeoutBlocks: 500,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true,     // Skip dry run before migrations? (default: false for public nets ),
